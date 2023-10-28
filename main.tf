2894262bc8d8b04b0ab24d3c0f7618d01d9d21df
shivasingam111 <shivasingam111@gmail.com>
2023-10-28 19:10:42 +0530

    modify vpc

--- main.tf
+++ main.tf
@@ -1,82 +1,46 @@
+provider "aws" {
+  region = "us-east-1" # Change this to your desired AWS region
+}
 
-# Create a VPC
 resource "aws_vpc" "custom_vpc" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+  cidr_block = "10.0.0.0/16"
 }
 
-# Create an internet gateway
-resource "aws_internet_gateway" "vpc_igw" {
-  vpc_id = aws_vpc.custom_vpc.id
-}
-
-# Create public and private subnets
-resource "aws_subnet" "public_subnets" {
-  count                   = length(var.azs)
+resource "aws_subnet" "subnet_a" {
+  count = 1
   vpc_id                  = aws_vpc.custom_vpc.id
-  cidr_block              = element(var.publicsubnet_cidr, count.index)
-  availability_zone       = element(var.azs, count.index)
+  cidr_block              = "10.0.1.0/24" # Change this CIDR block as needed
+  availability_zone       = "us-east-1a"  # Change to your desired availability zone
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "private_subnets" {
-  count             = length(var.azs)
-  vpc_id            = aws_vpc.custom_vpc.id
-  cidr_block        = element(var.privatesubnet_cidr, count.index)
-  availability_zone = element(var.azs, count.index)
+resource "aws_subnet" "subnet_b" {
+  count = 1
+  vpc_id                  = aws_vpc.custom_vpc.id
+  cidr_block              = "10.0.2.0/24" # Change this CIDR block as needed
+  availability_zone       = "us-east-1b"  # Change to your desired availability zone
+  map_public_ip_on_launch = true
 }
 
-# Create NAT gateways for private subnets
-resource "aws_eip" "nat_gw_ips" {
-  count = length(var.azs)
-}
-
-resource "aws_nat_gateway" "nat_gateways" {
-  count         = length(var.azs)
-  allocation_id = aws_eip.nat_gw_ips[count.index].id
-  subnet_id     = aws_subnet.public_subnets[count.index].id
-}
-
-# Create route tables for private subnets
-resource "aws_route_table" "private_route_tables" {
-  count  = 2
+resource "aws_internet_gateway" "example" {
   vpc_id = aws_vpc.custom_vpc.id
 }
 
-resource "aws_route" "private_subnet_routes" {
-  count                  = 2
-  route_table_id         = aws_route_table.private_route_tables[count.index].id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat_gateways[count.index].id
-}
-
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.custom_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
+    gateway_id = aws_internet_gateway.example.id
   }
 }
 
-resource "aws_route_table_association" "public_association" {
-  for_each      = aws_subnet.public_subnet
-  subnet_id      = each.value.id
-  route_table_id = aws_route_table.public_route_table.id
+resource "aws_route_table_association" "subnet_a" {
+  subnet_id      = aws_subnet.subnet_a[0].id
+  route_table_id = aws_route_table.public.id
 }
 
-# # Create a route table for each public subnet
-# resource "aws_route_table" "public_route_tables" {
-#   count = length(var.azs)
-#   vpc_id = aws_vpc.custom_vpc.id
-# }
-# # Create a route in each public subnet route table to direct traffic to the Internet Gateway
-# resource "aws_route" "public_subnet_routes" {
-#   count = 2
-#   route_table_id = aws_route_table.public_route_tables[count.index].id
-#   destination_cidr_block = "0.0.0.0/0"
-#   gateway_id = aws_internet_gateway.vpc_igw.id
-# }
-
-
+resource "aws_route_table_association" "subnet_b" {
+  subnet_id      = aws_subnet.subnet_b[0].id
+  route_table_id = aws_route_table.public.id
+}
